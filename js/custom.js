f92e0e675f435953e3391f48113c71472e68c765
weicheng xi <xiwc87@yeah.net>
2015-05-25 21:36:24 +0800

    ...

--- js/custom.js
+++ js/custom.js
@@ -8,71 +8,6 @@ jQuery(document).ready(function($) {
     var regexMd = /.*\.md/gi;
     var regexPdf = /.*\.pdf/gi;
 
-    /**
-     * Get page info from document, resize canvas accordingly, and render page.
-     * @param num Page number.
-     */
-    function renderPage(num) {
-        pageRendering = true;
-        // Using promise to fetch the page
-        pdfDoc.getPage(num).then(function(page) {
-            var viewport = page.getViewport(scale);
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-            // Render PDF page into canvas context
-            var renderContext = {
-                canvasContext: ctx,
-                viewport: viewport
-            };
-            var renderTask = page.render(renderContext);
-            // Wait for rendering to finish
-            renderTask.promise.then(function() {
-                pageRendering = false;
-                if (pageNumPending !== null) {
-                    // New page rendering is pending
-                    renderPage(pageNumPending);
-                    pageNumPending = null;
-                }
-            });
-        });
-        // Update page counters
-        document.getElementById('page_num').textContent = pageNum;
-    };
-
-    /**
-     * If another page rendering in progress, waits until the rendering is
-     * finised. Otherwise, executes rendering immediately.
-     */
-    function queueRenderPage(num) {
-        if (pageRendering) {
-            pageNumPending = num;
-        } else {
-            renderPage(num);
-        }
-    };
-
-    /**
-     * Displays previous page.
-     */
-    function onPrevPage() {
-        if (pageNum <= 1) {
-            return;
-        }
-        pageNum--;
-        queueRenderPage(pageNum);
-    };
-
-    /**
-     * Displays next page.
-     */
-    function onNextPage() {
-        if (pageNum >= pdfDoc.numPages) {
-            return;
-        }
-        pageNum++;
-        queueRenderPage(pageNum);
-    };
-
     $.getJSON("data/articles.json", function(data) {
         $.each(data.articles, function(i, item) {
             $('.ui.menu.sidebar').append($('<a class="item"/>').attr('data-href', item.path).text(item.name));
@@ -95,6 +30,79 @@ jQuery(document).ready(function($) {
                 $('.xwc-content').html('<canvas id="the-canvas" style="border:1px solid black;"/>');
                 $('.xwc-content').prepend('<div> <button id="prev">Previous</button> <button id="next">Next</button> &nbsp; &nbsp; <span>Page: <span id="page_num"></span> / <span id="page_count"></span></span> </div>')
 
+
+                var pdfDoc = null,
+                    pageNum = 1,
+                    pageRendering = false,
+                    pageNumPending = null,
+                    scale = 0.8,
+                    canvas = document.getElementById('the-canvas'),
+                    ctx = canvas.getContext('2d');
+
+                /**
+                 * Get page info from document, resize canvas accordingly, and render page.
+                 * @param num Page number.
+                 */
+                function renderPage(num) {
+                    pageRendering = true;
+                    // Using promise to fetch the page
+                    pdfDoc.getPage(num).then(function(page) {
+                        var viewport = page.getViewport(scale);
+                        canvas.height = viewport.height;
+                        canvas.width = viewport.width;
+                        // Render PDF page into canvas context
+                        var renderContext = {
+                            canvasContext: ctx,
+                            viewport: viewport
+                        };
+                        var renderTask = page.render(renderContext);
+                        // Wait for rendering to finish
+                        renderTask.promise.then(function() {
+                            pageRendering = false;
+                            if (pageNumPending !== null) {
+                                // New page rendering is pending
+                                renderPage(pageNumPending);
+                                pageNumPending = null;
+                            }
+                        });
+                    });
+                    // Update page counters
+                    document.getElementById('page_num').textContent = pageNum;
+                };
+
+                /**
+                 * If another page rendering in progress, waits until the rendering is
+                 * finised. Otherwise, executes rendering immediately.
+                 */
+                function queueRenderPage(num) {
+                    if (pageRendering) {
+                        pageNumPending = num;
+                    } else {
+                        renderPage(num);
+                    }
+                };
+
+                /**
+                 * Displays previous page.
+                 */
+                function onPrevPage() {
+                    if (pageNum <= 1) {
+                        return;
+                    }
+                    pageNum--;
+                    queueRenderPage(pageNum);
+                };
+
+                /**
+                 * Displays next page.
+                 */
+                function onNextPage() {
+                    if (pageNum >= pdfDoc.numPages) {
+                        return;
+                    }
+                    pageNum++;
+                    queueRenderPage(pageNum);
+                };
 
                 document.getElementById('prev').addEventListener('click', onPrevPage);
                 document.getElementById('next').addEventListener('click', onNextPage);
