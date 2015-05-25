f8664b60e56278463b5e44cf6ee4e75a734d60a9
weicheng xi <xiwc87@yeah.net>
2015-05-25 20:18:16 +0800

    markdown & pdf

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,10 @@ jQuery(document).ready(function($) {
 
     }).sidebar('attach events', '.xwc-menu');
 
+    var regexHtml = /.*\.html/gi;
+    var regexMd = /.*\.md/gi;
+    var regexPdf = /.*\.pdf/gi;
+
 
     $.getJSON("data/articles.json", function(data) {
         $.each(data.articles, function(i, item) {
@@ -12,7 +16,43 @@ jQuery(document).ready(function($) {
 
         $('.ui.menu.sidebar a.item').click(function(event) {
             $('.xwc-title').text($(this).text());
-            $('.xwc-content').load($(this).attr('data-href'));
+            var url = $(this).attr('data-href');
+
+            if (regexHtml.test(url)) {
+                $.get(url, function(data) {
+                    $('.xwc-content').text(data);
+                });
+            } else if (regexMd.test(url)) {
+                $.get(url, function(data) {
+                    $('.xwc-content').text(markdown.toHTML(data));
+                });
+            } else if (regexPdf.test(url)) {
+                $('.xwc-content').html('<canvas id="the-canvas" style="border:1px solid black;"/>')
+                PDFJS.getDocument(url).then(function(pdf) {
+                    // Using promise to fetch the page
+                    pdf.getPage(1).then(function(page) {
+                        var scale = 1.5;
+                        var viewport = page.getViewport(scale);
+
+                        //
+                        // Prepare canvas using PDF page dimensions
+                        //
+                        var canvas = document.getElementById('the-canvas');
+                        var context = canvas.getContext('2d');
+                        canvas.height = viewport.height;
+                        canvas.width = viewport.width;
+
+                        //
+                        // Render PDF page into canvas context
+                        //
+                        var renderContext = {
+                            canvasContext: context,
+                            viewport: viewport
+                        };
+                        page.render(renderContext);
+                    });
+                });
+            }
         });
 
     });
